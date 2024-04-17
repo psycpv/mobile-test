d44186bf9053ede841a5f051e3269f5506c4ef57
Nikola <nikolamitrovic368@gmail.com>
2024-04-18 01:47:57 +0200

    fwfwef

--- src/components/PwaProvider/index.tsx
+++ src/components/PwaProvider/index.tsx
@@ -21,21 +21,10 @@ export default function PwaProvider() {
   const isOpenPWAPrompt = useIsOpenPWAPrompt()
   useEffect(() => {
     // @ts-ignore: Property 'standalone' does not exist on type 'Navigator'.
-    // const isStandalone = window.navigator.standalone || window.matchMedia('(display-mode: standalone)').matches
+    const isStandalone = window.navigator.standalone || window.matchMedia('(display-mode: standalone)').matches
+    setIsInstalledPWA(isStandalone)
     window.addEventListener('beforeinstallprompt', handleBeforeInstallPromptEvent)
-    window.addEventListener('appinstalled', () => {
-      // If visible, hide the install promotion
-      alert('is installed')
-      // Log install to analytics
-      console.log('INSTALL: Success')
-    })
-    async function test() {
-      // @ts-ignore: Property 'getInstalledRelatedApps' does not exist on type 'Navigator'
-      const relatedApps = await navigator.getInstalledRelatedApps()
-      const PWAisInstalled = relatedApps.length > 0
-      setIsInstalledPWA(PWAisInstalled)
-    }
-    test()
+
     return function () {
       window.removeEventListener('beforeinstallprompt', handleBeforeInstallPromptEvent)
     }

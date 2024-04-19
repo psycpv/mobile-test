35d5cdf7648ad4ef5e44b91f1fa5f46dd7a081d6
joshua8321 <j.milo.mack@gmail.com>
2024-04-19 12:50:38 +0200

    fwef

--- src/components/PwaProvider/index.tsx
+++ src/components/PwaProvider/index.tsx
@@ -5,6 +5,31 @@ import { useIsOpenPWAPrompt, useSetIsInstalledPWA, useSetIsOpenPWAPrompt } from 
 import styled from 'styled-components'
 const PWAPrompt = dynamic(() => import('react-ios-pwa-prompt'), { ssr: false })
 
+const PWAPromptContainer = styled.div`
+  & > {
+    .iOSPWA-description-copy,
+    .iOSPWA-title,
+    .iOSPWA-step1-copy,
+    .iOSPWA-step2-copy,
+    .iOSPWA-step2-icon {
+      color: #131313 !important;
+    }
+
+    @media (prefers-color-scheme: dark) {
+      .iOSPWA-description-copy,
+      .iOSPWA-title,
+      .iOSPWA-step1-copy,
+      .iOSPWA-step2-copy,
+      .iOSPWA-step2-icon {
+        color: #fcfcfc !important;
+      }
+      .iOSPWA-container {
+        background-color: #1d1d1d !important;
+      }
+    }
+  }
+`
+
 const Title = styled.div`
   display: flex;
   align-items: center;
@@ -31,17 +56,20 @@ export default function PwaProvider() {
   }, [setIsInstalledPWA])
 
   return isOpenPWAPrompt ? (
-    <PWAPrompt
-      onClose={() => {
-        setIsOpenPWAPrompt(false)
-        localStorage.removeItem('iosPwaPrompt')
-      }}
-      copyTitle={
-        <Title>
-          <Image src="/images/192x192_App_Icon.png" alt="app icon" width={40} height={40} />
-          <div>Install Vibe Trading App</div>
-        </Title>
-      }
-    />
+    <PWAPromptContainer>
+      {' '}
+      <PWAPrompt
+        onClose={() => {
+          setIsOpenPWAPrompt(false)
+          localStorage.removeItem('iosPwaPrompt')
+        }}
+        copyTitle={
+          <Title>
+            <Image src="/images/192x192_App_Icon.png" alt="app icon" width={40} height={40} />
+            <div>Install Vibe Trading App</div>
+          </Title>
+        }
+      />
+    </PWAPromptContainer>
   ) : null
 }
